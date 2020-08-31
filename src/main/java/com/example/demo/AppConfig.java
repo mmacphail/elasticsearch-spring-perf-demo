0741c36f999a8ff22003779119e1a42955276737
mmacphail <alexandre.fruchaud@gmail.com>
2020-08-31 21:15:14 +0200

    Test with Reactor

--- src/main/java/com/example/demo/AppConfig.java
+++ src/main/java/com/example/demo/AppConfig.java
@@ -1,20 +1,67 @@
 package com.example.demo;
 
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorResourceFactory;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.resources.LoopResources;
+
+import javax.net.ssl.SSLException;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class AppConfig {
 
-    @Bean
-    AsyncTaskExecutor asyncTaskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(15);
-        executor.setMaxPoolSize(15);
-        executor.setQueueCapacity(15000);
-        return executor;
+    @Bean("my-nio-event")
+    NioEventLoopGroup getNioEventLoopGroup() {
+        BasicThreadFactory threadFactory = new BasicThreadFactory.Builder()
+                .namingPattern("TracesThread-%d")
+                .priority(Thread.MAX_PRIORITY)
+                .build();
+
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                5,
+                5,
+                0L,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(),
+                threadFactory,
+                new ThreadPoolExecutor.AbortPolicy()
+        );
+
+        return new NioEventLoopGroup(5, executor);
+    }
+
+    @Bean("my-rrf")
+    public ReactorResourceFactory reactorResourceFactory(@Qualifier("my-nio-event") NioEventLoopGroup group) {
+        ReactorResourceFactory f= new ReactorResourceFactory();
+        f.setLoopResources(b -> group);
+        f.setUseGlobalResources(false);
+        return f;
+    }
+
+    @Bean("my-rchc")
+    public ReactorClientHttpConnector reactorClientHttpConnector(@Qualifier("my-rrf") ReactorResourceFactory r) throws SSLException {
+        SslContext sslContext = SslContextBuilder
+                .forClient()
+                .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                .build();
+        return new ReactorClientHttpConnector(r, m -> m.secure(t -> t.sslContext(sslContext)));
+    }
+
+    @Bean("my-webClient")
+    public WebClient webClient(@Qualifier("my-rchc") ReactorClientHttpConnector r) {
+        return WebClient.builder().baseUrl("http://localhost:9200").clientConnector(r).build();
     }
 
 }
--- src/main/java/com/example/demo/DataLoader.java
+++ src/main/java/com/example/demo/DataLoader.java
@@ -3,16 +3,19 @@ package com.example.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 @Component
 public class DataLoader {
@@ -20,34 +23,36 @@ public class DataLoader {
     private static final Logger LOGGER = LoggerFactory.getLogger(DataLoader.class);
 
     @Autowired
-    AsyncTaskExecutor asyncTaskExecutor;
-
-    @Autowired
-    FlowRepository flowRepository;
+    @Qualifier("my-webClient")
+    private WebClient webClient;
 
     @PostConstruct
     public void init() {
         LOGGER.info("Start");
-        //.subscribe(flow -> LOGGER.info("Persisted flow"))
-        List<Future<Flow>> flows = IntStream.range(0, 10_000)
-                .mapToObj(i -> {
-                    return asyncTaskExecutor.submit(() -> {
-                        Flow flow = flowRepository.save(Flow.random()).block();
-                        return flow;
-                        //.subscribe(flow -> LOGGER.info("Persisted flow"))
-                    });
-                })
+
+        List<Mono<ClientResponse>> flows = IntStream.range(0, 10_000)
+                .mapToObj(i -> persistRandomFlow())
                 .collect(Collectors.toList());
 
-        flows.forEach(f -> {
-            try {
-                f.get();
-            } catch (InterruptedException | ExecutionException e) {
-                throw new RuntimeException(e);
+        flows.forEach(flow -> {
+            ClientResponse r = flow.block();
+            if(!r.statusCode().is2xxSuccessful()) {
+                r.bodyToFlux(String.class).subscribe(body -> {
+                    LOGGER.error("Received {} status: {}", r.statusCode(), body);
+                });
             }
         });
 
         LOGGER.info("End");
     }
 
+    private Mono<ClientResponse> persistRandomFlow() {
+        Flow flow = Flow.random();
+
+        return webClient.post()
+                .uri("/flows/_doc")
+                .body(BodyInserters.fromValue(flow))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .exchange();
+    }
 }
